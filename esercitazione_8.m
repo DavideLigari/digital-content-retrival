057d694832605264ac9f576ca9d468761cf4b86a
DavideLigari01 <davide.ligari01@universitadipavia.it>
2022-10-22 10:25:06 +0200

    fixed wrong exercises

--- esercitazione_8.m
+++ esercitazione_8.m
@@ -4,17 +4,58 @@
 % displaying the histograms on the three-color channel.
 clear;
 
-imm=imread("digitalContentRetrival/matlab/rgbImage.jpeg","jpeg");
+[file,path] = uigetfile({'*.tif;*.jpg;*.png;*.jpeg'},"select an image");
+imm=imread(strcat(path,file));
 
 in= input('insert the mean and variance of the gaussian noise in the following format, [mean,variance]');
 immWithNoise=imnoise(imm,'gaussian',in(1),in(2));
 
-figure(1)
+tmp=size(size(mat));
 
-subplot(1,2,1);
-title('image');
-imshow(imm);
+%if the image is rgb show the histograms of each colours
+if(tmp(2)==3)
+    figure(1)
+    
+    subplot(2,1,1);
+    title('image');
+    imshow(imm);
+    
+    subplot(2,1,2);
+    imhist(imm);
+    
+    figure(2)
+    subplot(2,3,2);
+    imshow(immWithNoise);
+    
+    subplot(2,3,4);
+    
+    imhist(immWithNoise(:,:,1));
+    myHist = findobj(gca, 'Type', 'Stem');
+    % Change the color to red
+    myHist.Color = [1 0 0];
+    
+    subplot(2,3,5);
+    imhist(immWithNoise(:,:,2));
+    myHist = findobj(gca, 'Type', 'Stem');
+    % Change the color to green
+    myHist.Color = [0 1 0];
+    
+    subplot(2,3,6);
+    imhist(immWithNoise(:,:,1));
+else
+    figure(1)
+    
+    subplot(2,2,1);
+    title('image');
+    imshow(imm);
 
-subplot(1,2,2);
-imshow(immWithNoise);
+    subplot(2,2,3);
+    imhist(imm);
 
+     subplot(2,2,2);
+    title('image');
+    imshow(immWithNoise);
+
+    subplot(2,2,4);
+    imhist(immWithNoise);
+end
--- esercitazione_9.m
+++ esercitazione_9.m
@@ -2,19 +2,14 @@
 % Write a program to compute the root-mean-square error of a compressed- decompressed
 % image.
 clear;
-
-original=imread("digitalContentRetrival/matlab/tifImage.tiff","tif");
-imwrite(original,'/home/liga/Scrivania/unipv/digitalContentRetrival/matlab/compressed.jpeg','jpeg');
-compressed=imread("digitalContentRetrival/matlab/compressed.jpeg","jpeg");
+[file,path] = uigetfile({'*.tiff;*.jpg;*.png;*.jpeg'},"select an image");
+original=imread(strcat(path,file));
+imwrite(original,strcat(path,'compressed.jpeg'),'jpeg');
+compressed=imread(strcat(path,"compressed.jpeg"),"jpeg");
 
 diff=original - compressed;
 diff= nonzeros(diff);
-square=diff.^2;
-sum=0;
+square= power(diff,2);
 n=size(square);
-for i=1:n(1)
-    sum=sum + square(i);
-end
-meanSquareError= sum/n(1);
-meanSquareError
-
+sum= sum(square);
+mse=sum/n(1);
