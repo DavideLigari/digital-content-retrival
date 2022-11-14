8ac23269d8db9e084d3f49578f7dd043ca1b303e
DavideLigari01 <davide.ligari01@universitadipavia.it>
2022-11-14 12:18:38 +0100

    added es 29 and 30

---
+++ esercitazione_29.m
@@ -0,0 +1,37 @@
+% ES 29 - 30
+% Choose a gray level image and create ten compressed version using jpeg format (hint:use
+% imwrite function with the format jpeg and increasing quality factor, from 10 to 100). Then
+% compute the psnr (between the original image and the compressed versions) and plot it
+% versus the image quality of jpeg.
+% Clear command window.
+clc;
+% Delete all variables.
+clear;
+% Close all figure windows except those created by imtool.
+close all;
+% Close all figure windows created by imtool.
+imtool close all;
+
+[file, path] = uigetfile({'*.tiff;*.jpg;*.png;*.jpeg'}, "select an image");
+image = imread(strcat(path, file));
+
+for i = 10:+10:100
+    imwrite(image, strcat(path, sprintf('image_jpeg_%i.jpeg', i)), "Quality", i);
+end
+
+figure(1)
+% Maximize figure.
+set(gcf, 'Position', get(0, 'Screensize'));
+
+for i = 10:+10:100
+    A = imread(strcat(path, sprintf('image_jpeg_%i.jpeg', i)));
+    subplot(2, 5, i / 10)
+    imshow(A);
+    set(get(gca, 'Title'), 'String', sprintf('jpeg compressed image with quality %i \n PSNR : %0.2f', i, psnr(A, image)));
+
+end
+
+for i = 10:+10:100
+    filename = strcat(path, sprintf('image_jpeg_%i.jpeg', i));
+    delete (filename);
+end
